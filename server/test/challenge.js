bea48a73a22c73b6b07442f108661186a6e39c6c
Cam Pedersen <diffference@gmail.com>
2013-03-25 19:53:11 +0000

    remove challenge/response tests

--- server/test/challenge.js
+++ server/test/challenge.js
@@ -32,7 +32,6 @@ describe("challenge", function () {
   };
 
   before(function () {
-    challenge = require('../lib/challenge');
     this.clock = sinon.useFakeTimers();
   });
 
@@ -40,32 +39,4 @@ describe("challenge", function () {
     this.clock.restore();
   });
 
-  describe("makeChallenge", function () {
-    it("generates a login challenge", sinon.test(function () {
-      var randomIdx = 0, randomVals = [
-        'cafefacecafefacecafefacecafefacecafefacecafefacecafefacecafeface',
-        'feedbeadfeedbeadfeedbeadfeedbead'
-      ];
-      function fakeRandom(len) {
-        var val = new Buffer(randomVals[randomIdx++], 'hex');
-        assert.strictEqual(
-          len, val.length,
-          'requested ' + len + ' bytes, but randomVals had ' + val.length
-        );
-        return val;
-      }
-      this.stub(challenge.__crypto, 'randomBytes', fakeRandom);
-      assert.deepEqual(challenge.makeChallenge(account), {
-        challenge: {
-          challengeKeySalt: account.challengeKeySalt,
-          iv: randomVals[1],
-          time: '0',
-          challenge: '576ad1186f9edc1f54b9eb5db5a4dc98'
-                   + '4bfaf2aa9b3cedbacb6c5752d3b8de9b'
-        },
-        answerDigest: '34fa6bec8b904d020c819ed0bfda3db7'
-                    + 'aa42947e13ae070c6b81503ca4036637'
-      });
-    }));
-  });
 });

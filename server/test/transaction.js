0af2f47975a3a8045acfd3d4a991f9d0b4f5ee07
Cam Pedersen <diffference@gmail.com>
2013-04-24 17:11:04 -0500

    initial transaction lib tests

--- server/test/transaction.js
+++ server/test/transaction.js
@@ -15,3 +15,46 @@
  * You should have received a copy of the Affero GNU General Public License
  * along with Crypton Server.  If not, see <http://www.gnu.org/licenses/>.
 */
+
+var assert = require('assert');
+var Transaction = require('../lib/transaction');
+
+describe('Transaction model', function () {
+  it('should create a blank transaction object', function () {
+    var tx = new Transaction();
+    assert(tx instanceof Transaction);
+  });
+
+  var token;
+  describe('#create()', function () {
+    it('should add a token to the object', function (done) {
+      var accountId = 2; // account from account lib tests
+      var tx = new Transaction();
+      tx.create(accountId, function (err) {
+        assert.equal(err, null);
+        assert.exist(tx.token);
+        token = tx.token;
+        done();
+      });
+    });
+  });
+
+  describe('#get()', function () {
+    it('should err on invalid token', function (done) {
+      var tx = new Transaction();
+      tx.get(666, function (err) {
+        assert.equal(err, null);
+        done();
+      });
+    });
+
+    it('should get the created transaction', function (done) {
+      var tx = new Transaction();
+      tx.get(token, function (err) {
+        assert.equal(err, null);
+        assert.equal(tx.numOperations, 0);
+        done();
+      });
+    });
+  });
+});

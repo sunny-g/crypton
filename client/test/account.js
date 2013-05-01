a52e81677544dea5b401d8f3b53c854bf9f4c442
Cam Pedersen <diffference@gmail.com>
2013-05-01 17:45:42 -0500

    add initial client test files

---
+++ client/test/account.js
@@ -0,0 +1,19 @@
+/* Crypton Client, Copyright 2013 SpiderOak, Inc.
+ *
+ * This file is part of Crypton Client.
+ *
+ * Crypton Client is free software: you can redistribute it and/or modify it
+ * under the terms of the Affero GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * Crypton Client is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the Affero GNU General Public
+ * License for more details.
+ *
+ * You should have received a copy of the Affero GNU General Public License
+ * along with Crypton Client.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+var assert = chai.assert;

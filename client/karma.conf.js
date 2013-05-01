c670256fbd8d771ef199cfdad2fe927f45e49af2
Cam Pedersen <diffference@gmail.com>
2013-05-01 18:11:46 -0500

    clean up karma config

--- client/karma.conf.js
+++ client/karma.conf.js
@@ -1,12 +1,5 @@
-// Karma configuration
-// Generated on Wed May 01 2013 16:56:51 GMT-0500 (CDT)
-
-
-// base path, that will be used to resolve files and exclude
 basePath = '';
 
-
-// list of files / patterns to load in the browser
 files = [
   MOCHA,
   MOCHA_ADAPTER,
@@ -15,36 +8,21 @@ files = [
   'test/*.js'
 ];
 
+exclude = [];
 
-// list of files to exclude
-exclude = [
-  
+reporters = [
+  'progress'
 ];
 
-
-// test results reporter to use
-// possible values: 'dots', 'progress', 'junit'
-reporters = ['progress'];
-
-
-// web server port
 port = 9876;
 
-
-// cli runner port
 runnerPort = 9100;
 
-
-// enable / disable colors in the output (reporters and logs)
 colors = true;
 
-
-// level of logging
 // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-logLevel = LOG_INFO;
+logLevel = LOG_DEBUG;
 
-
-// enable / disable watching file and executing tests whenever any file changes
 autoWatch = true;
 
 
@@ -64,10 +42,6 @@ browsers = [
 ];
 
 
-// If browser does not capture in given timeout [ms], kill it
 captureTimeout = 60000;
 
-
-// Continuous Integration mode
-// if true, it capture browsers, run tests and exit
 singleRun = false;

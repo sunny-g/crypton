e3d03260204b236f4c7c90661a46469fffea9a7b
Cam Pedersen <diffference@gmail.com>
2013-05-14 16:03:29 -0500

    basic account generation integration tests

--- integration_tests/test/core.js
+++ integration_tests/test/core.js
@@ -1,7 +1,25 @@
-(function () {
-describe('test', function () {
-  it('should do', function () {
-    assert(true);
+
+describe('Core functionality', function () {
+  describe('Account generation', function () {
+    it('should refuse registrations without a username', function (done) {
+      crypton.generateAccount('', '', function (err, account) {
+        assert.equal(err, 'Missing field: username');
+        done();
+      });
+    });
+
+    it('should accept registrations without a passphrase', function (done) {
+      crypton.generateAccount('notSoSmart', '', function (err, account) {
+        assert.equal(err, null);
+        done();
+      });
+    });
+
+    it('should registrations registrations without an existing username', function (done) {
+      crypton.generateAccount('notSoSmart', '', function (err, account) {
+        assert.equal(err, 'Username already taken.');
+        done();
+      });
+    });
   });
 });
-})();

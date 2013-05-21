9d37262d521489577ee47d1cd675b91e90f3261f
Cam Pedersen <diffference@gmail.com>
2013-05-21 17:15:43 -0500

    do not divulge server state in transaction update error message

--- server/lib/transaction.js
+++ server/lib/transaction.js
@@ -73,7 +73,9 @@ Transaction.prototype.update = function () {
 Transaction.prototype.add = function (data, callback) {
   var that = this;
   this.assertOwnership(callback, function () {
-    db.updateTransaction(that, data, callback);
+    db.updateTransaction(that, data, function (err) {
+      callback(err && 'Invalid chunk data');
+    });
   });
 };
 

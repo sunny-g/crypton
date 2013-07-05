86aa306e395a6c6fbd441cc592b16ea127ea9969
Cam Pedersen <diffference@gmail.com>
2013-07-05 10:20:17 -0500

    skeletal inbox/message API for client

---
+++ client/src/inbox.js
@@ -0,0 +1,60 @@
+/* Crypton Client, Copyright 2013 SpiderOak, Inc.
+ *
+ * This file is part of Crypton Client.
+ *
+ * Crypton Client is free software: you can redistribute it and/or modify it
+ * under the terms of the Affero GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * Crypton Client is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the Affero GNU General Public
+ * License for more details.
+ *
+ * You should have received a copy of the Affero GNU General Public License
+ * along with Crypton Client.  If not, see <http://www.gnu.org/licenses/>.
+*/
+(function() {
+  var Inbox = crypton.Inbox = function Inbox () {};
+
+  Inbox.prototype.poll = function () {
+  };
+
+  Inbox.prototype.messagesByPeer = function (peer) {
+  };
+
+  Inbox.prototype.list = function () {
+  };
+
+  Inbox.prototype.filter = function () {
+  };
+
+  Inbox.prototype.get = function () {
+  };
+
+  Inbox.prototype.delete = function () {
+  };
+
+  Inbox.prototype.clear = function () {
+  };
+
+
+  function Message () {
+    // id
+    // from
+    // to
+    // timestamp
+    // size
+    // ttl
+  };
+
+  Message.prototype.getHeaders = function () {
+  };
+
+  Message.prototype.getBody = function () {
+  };
+
+  Message.prototype.delete = function () {
+  };
+})();

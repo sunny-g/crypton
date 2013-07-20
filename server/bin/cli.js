1eb40a7695f45228776dae27aba9a0b231632ad4
Cam Pedersen <diffference@gmail.com>
2013-07-19 22:53:34 -0500

    bump the version

--- server/bin/cli.js
+++ server/bin/cli.js
@@ -23,7 +23,7 @@ var fs = require('fs');
 var program = require('commander');
 
 program
-  .version('0.0.1')
+  .version('0.0.2')
   .option('-c, --config [file]', 'Specify a custom configuration file [default config]')
   .parse(process.argv);
 

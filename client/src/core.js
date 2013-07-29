6ff097ed0840b26b0eca4ed4b043ec0ea8dc23f7
Cam Pedersen <diffference@gmail.com>
2013-07-29 13:29:24 -0500

    Complete client documentation

--- client/src/core.js
+++ client/src/core.js
@@ -18,109 +18,162 @@
 var crypton = {};
 
 (function () {
-  crypton.version = '0.0.1';
-  crypton.host = window.location.hostname;
-  crypton.cipherOptions = {
-    mode: 'gcm'
-  };
 
-  crypton.url = function () {
-    return 'https://' + crypton.host;
-  };
+/**!
+ * ### version
+ * Holds framework version for potential future backward compatibility
+ */
+crypton.version = '0.0.2';
 
-  function randomBytes (nbytes) {
-    return sjcl.random.randomWords(nbytes);
+/**!
+ * ### host
+ * Holds location of Crypton server
+ */
+crypton.host = window.location.hostname;
+
+/**!
+ * ### cipherOptions
+ * Sets AES mode to GCM, necessary for SJCL
+ */
+crypton.cipherOptions = {
+  mode: 'gcm'
+};
+
+/**!
+ * ### url()
+ * Generate URLs for server calls
+ * 
+ * @return {String} url
+ */
+crypton.url = function () {
+  return 'https://' + crypton.host;
+};
+
+/**!
+ * ### randomBytes()
+ * Generate `nbytes` bytes of random data
+ * 
+ * @param {Number} nbytes
+ */
+function randomBytes (nbytes) {
+  return sjcl.random.randomWords(nbytes);
+}
+crypton.randomBytes = randomBytes;
+
+/**!
+ * ### generateAccount(username, passphrase, callback, options)
+ * Generate salts and keys necessary for an account
+ *
+ * Saves account to server unless `options.save` is falsey
+ *
+ * Calls back with account and without error if successful
+ *
+ * Calls back with error if unsuccessful
+ * 
+ * @param {String} username
+ * @param {String} passphrase
+ * @param {Function} callback
+ * @param {Object} options
+ */
+// TODO consider moving non-callback arguments to single object
+crypton.generateAccount = function (username, passphrase, callback, options) {
+  options = options || {};
+  var keypairCurve = options.keypairCurve || 384;
+  var save = typeof options.save !== 'undefined' ? options.save : true;
+
+  var account = new crypton.Account();
+  var containerNameHmacKey = randomBytes(8);
+  var hmacKey = randomBytes(8);
+  var keypairSalt = randomBytes(8);
+  var keypair = sjcl.ecc.elGamal.generateKeys(keypairCurve, 0);
+  var symkey = keypair.pub.kem(0);
+  var challengeKeySalt = randomBytes(8);
+  var keypairKey = sjcl.misc.pbkdf2(passphrase, keypairSalt);
+
+  account.username = username;
+  account.challengeKeySalt = JSON.stringify(challengeKeySalt);
+  account.challengeKey = JSON.stringify(sjcl.misc.pbkdf2(passphrase, challengeKeySalt));
+  account.keypairSalt = JSON.stringify(keypairSalt);
+  account.keypairCiphertext = sjcl.encrypt(keypairKey, JSON.stringify(keypair.sec.serialize()), crypton.cipherOptions);
+  account.containerNameHmacKeyCiphertext = sjcl.encrypt(symkey.key, JSON.stringify(containerNameHmacKey), crypton.cipherOptions);
+  account.hmacKeyCiphertext = sjcl.encrypt(symkey.key, JSON.stringify(hmacKey), crypton.cipherOptions);
+  account.pubKey = JSON.stringify(keypair.pub.serialize());
+  account.symKeyCiphertext = JSON.stringify(symkey.tag);
+
+  if (save) {
+    account.save(function (err) {
+      callback(err, account);
+    });
+    return;
   }
-  crypton.randomBytes = randomBytes;
 
-  crypton.generateAccount = function (username, passphrase, callback, options) {
-    options = options || {};
-    var keypairCurve = options.keypairCurve || 384;
-    var save = typeof options.save !== 'undefined' ? options.save : true;
+  callback(null, account);
+};
 
-    var account = new crypton.Account();
-    var containerNameHmacKey = randomBytes(8);
-    var hmacKey = randomBytes(8);
-    var keypairSalt = randomBytes(8);
-    var keypair = sjcl.ecc.elGamal.generateKeys(keypairCurve, 0);
-    var symkey = keypair.pub.kem(0);
-    var challengeKeySalt = randomBytes(8);
-    var keypairKey = sjcl.misc.pbkdf2(passphrase, keypairSalt);
-
-    account.username = username;
-    account.challengeKeySalt = JSON.stringify(challengeKeySalt);
-    account.challengeKey = JSON.stringify(sjcl.misc.pbkdf2(passphrase, challengeKeySalt));
-    account.keypairSalt = JSON.stringify(keypairSalt);
-    account.keypairCiphertext = sjcl.encrypt(keypairKey, JSON.stringify(keypair.sec.serialize()), crypton.cipherOptions);
-    account.containerNameHmacKeyCiphertext = sjcl.encrypt(symkey.key, JSON.stringify(containerNameHmacKey), crypton.cipherOptions);
-    account.hmacKeyCiphertext = sjcl.encrypt(symkey.key, JSON.stringify(hmacKey), crypton.cipherOptions);
-    account.pubKey = JSON.stringify(keypair.pub.serialize());
-    account.symKeyCiphertext = JSON.stringify(symkey.tag);
-
-    if (save) {
-      account.save(function (err) {
-        callback(err, account);
-      });
-      return;
-    }
-
-    callback(null, account);
-  };
-
-  crypton.authorize = function (username, passphrase, callback) {
-    superagent.post(crypton.url() + '/account/' + username)
-      .end(function (res) {
-        if (!res.body || res.body.success !== true) {
-          callback(res.body.error);
-          return;
-        }
-
-        var body = res.body;
-        var response = {};
-        response.challengeKey = sjcl.misc.pbkdf2(passphrase, body.challengeKeySalt);
-
-        superagent.post(crypton.url() + '/account/' + username + '/answer')
-          .send(response)
-          .end(function (res) {
-            if (!res.body || res.body.success !== true) {
-              callback(res.body.error);
-              return;
-            }
-
-            var sessionIdentifier = res.body.sessionIdentifier;
-            var session = new crypton.Session(sessionIdentifier);
-            session.account = new crypton.Account();
-            session.account.passphrase = passphrase;
-            for (var i in res.body.account) {
-              session.account[i] = res.body.account[i];
-            }
-
-            session.account.unravel(function () {
-              var socket = crypton.io = io.connect(crypton.url(), {
-                secure: true
-              });
-
-              socket.on('message', function (data) {
-                //var peer = session.peers[data.from];
-                var headers = sjcl.decrypt(session.account.secretKey, data.headers, crypton.cipherOptions);
-                var body = sjcl.decrypt(session.account.secretKey, data.body, crypton.cipherOptions);
-
-                session.emit('message', {
-                  headers: JSON.parse(headers),
-                  body: JSON.parse(body),
-                  from: data.from
-                });
-              });
-
-              callback(null, session);
-            });
-          });
+/**!
+ * ### authorize(username, passphrase, callback)
+ * Perform zero-knowledge authorization with given `username`
+ * and `passphrase`, generating a session if successful
+ *
+ * Calls back with session and without error if successful
+ *
+ * Calls back with error if unsuccessful
+ * 
+ * @param {String} username
+ * @param {String} passphrase
+ * @param {Function} callback
+ */
+crypton.authorize = function (username, passphrase, callback) {
+  superagent.post(crypton.url() + '/account/' + username)
+    .end(function (res) {
+      if (!res.body || res.body.success !== true) {
+        callback(res.body.error);
+        return;
       }
-    );
-  };
 
-  crypton.resurrect = function () {
+      var body = res.body;
+      var response = {};
+      response.challengeKey = sjcl.misc.pbkdf2(passphrase, body.challengeKeySalt);
 
-  };
+      superagent.post(crypton.url() + '/account/' + username + '/answer')
+        .send(response)
+        .end(function (res) {
+          if (!res.body || res.body.success !== true) {
+            callback(res.body.error);
+            return;
+          }
+
+          var sessionIdentifier = res.body.sessionIdentifier;
+          var session = new crypton.Session(sessionIdentifier);
+          session.account = new crypton.Account();
+          session.account.passphrase = passphrase;
+          for (var i in res.body.account) {
+            session.account[i] = res.body.account[i];
+          }
+
+          session.account.unravel(function () {
+            var socket = crypton.io = io.connect(crypton.url(), {
+              secure: true
+            });
+
+            socket.on('message', function (data) {
+              //var peer = session.peers[data.from];
+              var headers = sjcl.decrypt(session.account.secretKey, data.headers, crypton.cipherOptions);
+              var body = sjcl.decrypt(session.account.secretKey, data.body, crypton.cipherOptions);
+
+              session.emit('message', {
+                headers: JSON.parse(headers),
+                body: JSON.parse(body),
+                from: data.from
+              });
+            });
+
+            callback(null, session);
+          });
+        });
+    }
+  );
+};
+
 })();
 
--- client/src/peer.js
+++ client/src/peer.js
@@ -16,81 +16,124 @@
  * along with Crypton Client.  If not, see <http://www.gnu.org/licenses/>.
 */
 (function () {
-  var Peer = crypton.Peer = function (options) {
-    options = options || {};
 
-    this.accountId = options.id;
-    this.session = options.session;
-    this.username = options.username;
-    this.pubKey = options.pubKey;
-  };
+/**!
+ * # Peer(options)
+ *
+ * ````
+ * var options = {
+ *   username: 'friend' // required
+ * };
+ *
+ * var peer = new crypton.Peer(options);
+ * ````
+ *
+ * @param {Object} options
+ */
+var Peer = crypton.Peer = function (options) {
+  options = options || {};
 
-  Peer.prototype.fetch = function (callback) {
-    if (!this.username) {
-      callback('Must supply peer username');
+  this.accountId = options.id;
+  this.session = options.session;
+  this.username = options.username;
+  this.pubKey = options.pubKey;
+};
+
+/**!
+ * ### fetch(callback)
+ * Retrieve peer data from server, applying it to parent object
+ *
+ * Calls back with peer data and without error if successful
+ *
+ * Calls back with error if unsuccessful
+ * 
+ * @param {Function} callback
+ */
+Peer.prototype.fetch = function (callback) {
+  if (!this.username) {
+    callback('Must supply peer username');
+    return;
+  }
+
+  if (!this.session) {
+    callback('Must supply session to peer object');
+    return;
+  }
+
+  var that = this;
+  var url = crypton.url() + '/peer/' + this.username;
+  superagent.get(url)
+    .set('session-identifier', this.session.id)
+    .end(function (res) {
+    if (!res.body || res.body.success !== true) {
+      callback(res.body.error);
       return;
     }
 
-    if (!this.session) {
-      callback('Must supply session to peer object');
+    var peer = res.body.peer;
+    that.accountId = peer.accountId;
+    that.username = peer.username;
+    that.pubKey = peer.pubKey;
+    // this may be necessary
+    var point = sjcl.ecc.curves['c' + peer.pubKey.curve].fromBits(peer.pubKey.point);
+    that.pubKey = new sjcl.ecc.elGamal.publicKey(peer.pubKey.curve, point.curve, point);
+
+    callback(null, that);
+  });
+};
+
+/**!
+ * ### encrypt(payload)
+ * Encrypt `message` with peer's public key
+ * 
+ * @param {Object} payload
+ * @return {Object} ciphertext
+ */
+Peer.prototype.encrypt = function (payload) {
+  // should this be async to callback with an error if there is no pubkey?
+  var ciphertext = sjcl.encrypt(this.pubKey, JSON.stringify(payload), crypton.cipherOptions);
+  return ciphertext;
+};
+
+/**!
+ * ### sendMessage(headers, body, callback)
+ * Encrypt `headers` and `body` and send them to peer in one logical `message`
+ * 
+ * Calls back with message id and without error if successful
+ *
+ * Calls back with error if unsuccessful
+ * 
+ * @param {Object} headers
+ * @param {Object} body
+ */
+Peer.prototype.sendMessage = function (headers, body, callback) {
+  if (!this.session) {
+    callback('Must supply session to peer object');
+    return;
+  }
+
+  var headerCiphertext = this.encrypt(headers);
+  var bodyCiphertext = this.encrypt(body);
+
+  var message = {
+    headers: headerCiphertext,
+    body: bodyCiphertext,
+    toAccount: this.accountId,
+  };
+
+  var url = crypton.url() + '/peer';
+  superagent.post(url)
+    .send(message)
+    .set('session-identifier', this.session.id)
+    .end(function (res) {
+    if (!res.body || res.body.success !== true) {
+      callback(res.body.error);
       return;
     }
 
-    var that = this;
-    var url = crypton.url() + '/peer/' + this.username;
-    superagent.get(url)
-      .set('session-identifier', this.session.id)
-      .end(function (res) {
-      if (!res.body || res.body.success !== true) {
-        callback(res.body.error);
-        return;
-      }
+    callback(null, res.body.messageId);
+  });
+};
 
-      var peer = res.body.peer;
-      that.accountId = peer.accountId;
-      that.username = peer.username;
-      that.pubKey = peer.pubKey;
-      // this may be necessary
-      var point = sjcl.ecc.curves['c' + peer.pubKey.curve].fromBits(peer.pubKey.point);
-      that.pubKey = new sjcl.ecc.elGamal.publicKey(peer.pubKey.curve, point.curve, point);
-
-      callback(null, that);
-    });
-  };
-
-  Peer.prototype.encrypt = function (message) {
-    // should this be async to return an error if there is no pubkey?
-    var ciphertext = sjcl.encrypt(this.pubKey, JSON.stringify(message), crypton.cipherOptions);
-    return ciphertext;
-  };
-
-  Peer.prototype.sendMessage = function (headers, body, callback) {
-    if (!this.session) {
-      callback('Must supply session to peer object');
-      return;
-    }
-
-    var headerCiphertext = this.encrypt(headers);
-    var bodyCiphertext = this.encrypt(body);
-
-    var message = {
-      headers: headerCiphertext,
-      body: bodyCiphertext,
-      toAccount: this.accountId,
-    };
-
-    var url = crypton.url() + '/peer';
-    superagent.post(url)
-      .send(message)
-      .set('session-identifier', this.session.id)
-      .end(function (res) {
-      if (!res.body || res.body.success !== true) {
-        callback(res.body.error);
-        return;
-      }
-
-      callback(null, res.body.messageId);
-    });
-  };
 })();
 

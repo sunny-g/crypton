54aed4bc1114f0bccfa05e749bf25f95f0299062
Cam Pedersen <diffference@gmail.com>
2013-08-02 12:37:35 -0500

    comment server/lib/sockets

--- server/lib/sockets.js
+++ server/lib/sockets.js
@@ -1,3 +1,20 @@
+/* Crypton Server, Copyright 2013 SpiderOak, Inc.
+ *
+ * This file is part of Crypton Server.
+ *
+ * Crypton Server is free software: you can redistribute it and/or modify it
+ * under the terms of the Affero GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * Crypton Server is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the Affero GNU General Public
+ * License for more details.
+ *
+ * You should have received a copy of the Affero GNU General Public License
+ * along with Crypton Server.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 'use strict';
 
@@ -11,6 +28,12 @@ app.log('info', 'starting socket.io');
 app.io = io.listen(app.server);
 app.clients = {};
 
+/**!
+ * Configure socket.io connection autorization
+ *
+ * Looks up the provided session to make sure it is valid
+ */
+// TODO is this necessary? we're retreiving the session on('connection')
 app.io.set('authorization', function (handshakeData, accept) {
   app.log('debug', 'authorizing websocket connection');
 
@@ -33,8 +56,14 @@ app.io.set('authorization', function (handshakeData, accept) {
   accept(null, true);
 });
 
+/**!
+ * Verify session, add session's accountId to socket handle,
+ * and add handle to app.clients so we can look it up easily.
+ * Remove handle from app.clients upon disconnection
+ */
 app.io.sockets.on('connection', function (socket) {
   var sid = socket.handshake.sessionId;
+
   app.sessionStore.get(sid, function (err, session) {
     if (err || !session) {
       // reconnect after server died and flushed sessions

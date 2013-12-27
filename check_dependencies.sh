13ccc777b2b5f5f6fb5893204a82691f10c349d6
Ken Manheimer <ken.manheimer@gmail.com>
2013-12-27 18:07:21 -0500

    Fix check_dependencies.sh so it works on recent ubuntu linux as well as mac.
    Among many fixes, the psql postgres user test now users 'sudo', which
    was not used in any of the tests before. I believe this is necessary for
    ubuntu.

--- check_dependencies.sh
+++ check_dependencies.sh
@@ -1,16 +1,15 @@
 #! /bin/sh
 
-#postgresql checks 
-POSTGRES_INSTALLED=$(which psql)
-if [ $POSTGRES_INSTALLED >/dev/null ]; then
+#postgresql checks
+if which psql > /dev/null; then
     echo "*** Found psql"
 else 
     echo ">>> Postgres is not installed, psql not located"
     exit 1;
 fi
 
-POSTGRES_VERSION=$(psql -V | grep -o 9)
-if [ $POSTGRES_VERSION == "9"  ]; then
+POSTGRES_VERSION="$(psql -V | grep -o 9)"
+if [ "$POSTGRES_VERSION" = "9" ]; then
     echo "*** Found postgres 9.x"
 else 
     echo ">>> Postgres 9.x is required for Crypton"
@@ -19,8 +18,8 @@ else
     exit 1;
 fi
 
-POSTGRES_USER_EXISTS=$(psql --user postgres -c "\du" | grep -o postgres)
-if [ $POSTGRES_USER_EXISTS == "postgres" ]; then
+POSTGRES_USER_EXISTS="$(sudo -u postgres psql --user postgres -c "\du" | grep -o postgres)"
+if [ "$POSTGRES_USER_EXISTS" = "postgres" ]; then
     echo "*** Postgres is setup correctly for Crypton"
 else
     echo ">>> Configuration issue: the postgres user does not exist, please create it:"
@@ -30,16 +29,21 @@ else
 fi
 
 # node checks
-NODE_INSTALLED=$(which node)
-if [ $NODE_INSTALLED >/dev/null ]; then
-    echo "*** Found Node.js"
-else 
-    echo ">>> Node.js is not installed, `which node` failed"
+# Try node first as nodejs, for systems (like ubuntu) where 'node' is the
+# amateur packet radio program:
+if which nodejs > /dev/null; then
+    NODECMD=nodejs
+    echo "*** Found Node.js as $NODECMD"
+elif which node > /dev/null; then
+    NODECMD=node
+    echo "*** Found Node.js as $NODECMD"
+else
+    echo ">>> Node.js is not installed, `which nodejs` and `which node` failed"
     exit 1;
 fi
 
-NODE_VERSION=$(node --version | grep -o "0.10.")
-if [ $NODE_VERSION == "0.10."  ]; then
+NODE_VERSION="$($NODECMD --version | grep -o "0.10.")"
+if [ "$NODE_VERSION" = "0.10."  ]; then
     echo "*** Found node 0.10.x"
 else 
     echo ">>> Node.js 0.10.x is required for Crypton"
@@ -48,8 +52,7 @@ else
 fi
 
 # redis-server checks
-REDIS_INSTALLED=$(which redis-server)
-if [ $REDIS_INSTALLED >/dev/null ]; then
+if which redis-server >/dev/null; then
     echo "*** Found redis-server"
 else 
     echo ">>> Redis is not installed, `which redis-server` failed"
@@ -57,7 +60,7 @@ else
 fi
 
 REDIS_VERSION=$(redis-server --version | grep -o "2.6")
-if [ $REDIS_VERSION == "2.6"  ]; then
+if [ "$REDIS_VERSION" = "2.6"  ]; then
     echo "*** Found redis 2.6.x"
 else 
     echo ">>> Redis 2.6.x is required for Crypton"

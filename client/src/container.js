b858c69ad86d1bd61994df38eb86028049375757
Cam Pedersen <diffference@gmail.com>
2014-01-18 20:02:40 -0600

    cache containerNameHmac

--- client/src/container.js
+++ client/src/container.js
@@ -193,9 +193,14 @@ Container.prototype.latestVersion = function () {
  * @return {String} hmac
  */
 Container.prototype.getPublicName = function () {
+  if (this.containerNameHmac) {
+    return this.containerNameHmac;
+  }
+
   var hmac = new sjcl.misc.hmac(this.session.account.containerNameHmacKey);
   var containerNameHmac = hmac.encrypt(this.name);
-  return sjcl.codec.hex.fromBits(containerNameHmac);
+  this.containerNameHmac = sjcl.codec.hex.fromBits(containerNameHmac);
+  return this.containerNameHmac;
 };
 
 /**!

0e5bea461a44c7e1b39298464a25baa73e251cae
Cam Pedersen <cam@campedersen.com>
2014-05-16 19:51:50 -0500

    add command line functionality for daemonization

--- server/bin/cli.js
+++ server/bin/cli.js
@@ -35,17 +35,137 @@
 
 var fs = require('fs');
 var program = require('commander');
+var forever = require('forever');
 
 program
   .version('0.0.2')
   .option('-c, --config [file]', 'Specify a custom configuration file [default config]')
-  .option('-d, --docker', 'Enable docker mode to use environment variables rather than a config file [default false]')
-  .parse(process.argv);
+  .option('-d, --docker', 'Enable docker mode to use environment variables rather than a config file [default false]');
 
-process.configFile = program.config;
-process.docker = program.docker;
+program.command('run')
+  .description('Run the Crypton server')
+  .action(function () {
+    process.configFile = program.config;
+    process.docker = program.docker;
+    var app = require('../app');
+    app.start();
+  });
 
-if (require.main === module) {
-  var app = require('../app');
-  app.start();
+program.command('status')
+  .description('Print the status of the daemonized the Crypton server')
+  .action(function () {
+    getServer(function (data) {
+      if (!data) {
+        console.log('No Crypton server daemon is running');
+      } else {
+        console.log(data);
+      }
+    });
+  });
+
+program.command('start')
+  .description('Daemonize the Crypton server')
+  .action(function () {
+    getServer(function (data) {
+      if (data) {
+        console.log('Crypton already running');
+        process.exit(1);
+      }
+
+      var monitor = forever.startDaemon(__filename, {
+        options: [ 'run' ]
+      });
+
+      console.log('Crypton server started');
+    });
+  });
+
+program.command('stop')
+  .description('Stop the daemonized Crypton server')
+  .action(function () {
+    getServer(function (data) {
+      if (!data) {
+        console.log('Crypton is not running');
+        process.exit(1);
+      }
+
+      forever.stop(data.index);
+      console.log('Crypton server stopped');
+    });
+  });
+
+program.command('restart')
+  .description('Restart the daemonized Crypton server')
+  .action(function () {
+    getServer(function (data) {
+      if (!data) {
+        console.log('Crypton is not running');
+        process.exit(1);
+      }
+
+      forever.restart(data.index);
+      console.log('Crypton server restarted');
+    });
+  });
+
+program.command('logs')
+  .description('Print the latest Crypton server logs')
+  .action(function () {
+    getServer(function (data) {
+      if (!data) {
+        console.log('Crypton is not running');
+        process.exit(1);
+      }
+
+      forever.tail(data.index, {}, function (err, log) {
+        console.log(log.line);
+      });
+    });
+  });
+
+program.command('tail')
+  .description('Tail Crypton server logs to stdout')
+  .action(function () {
+    getServer(function (data) {
+      if (!data) {
+        console.log('Crypton is not running');
+        process.exit(1);
+      }
+
+      forever.tail(data.index, {
+        stream: true
+      }, function (err, log) {
+        console.log(log.line);
+      });
+    });
+  });
+
+program.command('cleanlogs')
+  .description('Remove the log files')
+  .action(function () {
+    forever.cleanUp(true);
+  });
+
+program.parse(process.argv);
+
+if (!program.args[0]) {
+  program.help();
+}
+
+function getServer (callback) {
+  forever.list(false, function (err, data) {
+    if (err) {
+      console.log(err);
+      process.exit(1);
+    }
+
+    for (var i in data) {
+      if (~data[i].file.indexOf('crypton/server/bin/cli.js')) {
+        data[i].index = i;
+        return callback(data[i]);
+      }
+    }
+
+    callback(null);
+  });
 }

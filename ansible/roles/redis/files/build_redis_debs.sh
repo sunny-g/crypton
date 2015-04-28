18481675514bd15532780665f754c7d95558eba5
David Zuwenden <dhain@zognot.org>
2015-04-28 11:43:07 -0500

    update node, redis, postgres

--- ansible/roles/redis/files/build_redis_debs.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [[ -e deb/redis ]]; then 
-    rm -rf deb/redis
-fi
-mkdir -p deb/redis
-cd deb/redis
-apt-get source -b redis-server
-#DEBIAN_FRONTEND=noninteractive dpkg -i *.deb

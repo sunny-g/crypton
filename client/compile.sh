25f51ed9fdd875d4b400a49e17a965b404ca0cd4
David Dahl <ddahl@nulltxt.se>
2015-08-19 20:53:26 -0500

    Change license to MPL 2 as it is copyleft and iOS friendly

--- client/compile.sh
+++ client/compile.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+# Crypton Client, Copyright 2013, 2014, 2015 SpiderOak, Inc.
+#
+# This file is part of Crypton Client.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # cross-platform way to grab this script's directory
 pushd `dirname $0` > /dev/null
 SCRIPT_PATH=`pwd -P`
